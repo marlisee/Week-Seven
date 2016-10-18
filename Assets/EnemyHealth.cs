9ba480e20958a20172f564a4b1de3fc545f572c8
Marlise Chu <marlise@marlise-2.local>
2016-10-18 02:28:21 -0400

    commit

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+using UnityStandardAssets.Characters.FirstPerson;
+public class EnemyHealth : MonoBehaviour
+{
+	public int health = 100;
+	public GameObject JailCell;
+	public Text txt;
+	public gun gun;
+
+	void Start() {
+		JailCell.SetActive (false);
+	}
+	void Update()
+	{
+		if (health <= 0)
+		{
+			Dead ();
+			StartCoroutine (restart());
+		}
+	}
+	public void ApplyDamage(int damageToTake)
+	{
+		damageToTake = 100;
+		health -= damageToTake;
+	}
+	void Dead()
+	{
+		transform.Rotate(Vector3.left);
+		JailCell.SetActive(true);
+		txt.gameObject.SetActive (true);
+		GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = false;
+		gun.canwin = false;
+	}
+	IEnumerator restart()
+	{
+		yield return new  WaitForSeconds (4);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/gun.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class gun : MonoBehaviour {
+
+
+	public int theDamage;
+	public ParticleSystem muzzle;
+	public ParticleSystem trail;
+	public AudioClip fire;
+	public AudioSource src;
+	public float waitTime;
+	public Text txt;
+	public bool canwin;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Win());
+		canwin = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		EnemyHealth healthComponent;
+
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2,0));
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			muzzle.Emit (Random.Range (0, 60));
+			trail.Emit (1);
+			src.PlayOneShot (fire);
+			if(Physics.Raycast(ray,out hit,100))
+			{
+				// gets a reference to the component!
+				healthComponent = hit.collider.GetComponent<EnemyHealth>();
+
+				// just for safety: Check that you actually found EnemyHealth component on the raycast hit, if not do not proceed
+				if (healthComponent == null)
+					return;
+
+				healthComponent.ApplyDamage(theDamage);
+
+			}
+		}
+	}
+	IEnumerator Win()
+	{
+		
+		yield return new WaitForSeconds(10);
+		if(canwin)
+		txt.gameObject.SetActive (true);
+		yield return new WaitForSeconds (4);
+		if(canwin)
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
